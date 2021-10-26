cbe9f88ce46f58275d37af8188356cfb2da90b51
Artyom Levykh <art.lv@hotmail.com>
2021-10-26 21:19:38 +0300

    lambda пояснения

--- Lambda_функции.py
+++ Lambda_функции.py
@@ -1,3 +1,4 @@
+"""
 import time
 
 def gms():
@@ -24,10 +25,24 @@ root.title("Угадай слово")      # Устанавливаем заго
 root.geometry(f"{810}x{320}+{root.winfo_screenwidth() // 2 - 810 // 2}+{root.winfo_screenheight() // 2 - 320 // 2}")
 
 def pressButton():
-    print("Кнопка нажата")
+    print(lambda : 3 * 5)
 
 myButton = Button(text="Кнопка", width=25)
 myButton.place(x=10, y=10)
-myButton["command"] = pressButton                     # ["command"] - это свойство которое требует объект
+myButton["command"] = pressButton                   # ["command"] - это свойство которое требует объект
 
 root.mainloop()
+
+print("\n")
+# =======================================
+"""
+
+def kvadrat(n):
+    return n * n
+a = []
+for i in range(10):
+    a.append(lambda x = i: kvadrat(x))
+
+print(a[5]())               # Вывод на экран функции хранящейся в элементе с индексом 5 (не результат а именно функция!)
+for i in range(10):
+    print(a[i])
