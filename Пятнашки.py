3a0e00b6bfc67ddc85c31a3c1e093be2895565ab
Artyom Levykh <art.lv@hotmail.com>
2021-10-30 21:20:18 +0300

    Настройка геометрии окна

--- Пятнашки.py
+++ Пятнашки.py
@@ -4,3 +4,24 @@ from tkinter import messagebox      # Для использования окон
 from random import randint          # Случайные числа
 from winsound import Beep           # Простейший генератор звука
 
+# ============================= НАЧАЛО ПРОГРАММЫ ==========================================
+
+root = Tk()
+root.resizable(False, False)
+root.title("Головоломка для отчаянных и прокажённых")
+root.iconbitmap("icon/iconi.ico")
+
+# ЦВЕТА
+back = "#373737"   # Фон
+fore = "AFAFAF"   # Шрифт
+
+WIDTH = 422
+HEIGHT = 730
+POS_X = root.winfo_screenwidth() // 2 - WIDTH // 2
+POS_Y = root.winfo_screenheight() // 2 - HEIGHT // 2
+root.geometry(f"{WIDTH}x{HEIGHT}+{POS_X}+{POS_Y}")
+root["bg"] = back
+
+
+
+root.mainloop()
