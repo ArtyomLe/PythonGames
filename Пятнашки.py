1047533cea3944b96fd1cdc1ab98959b2282950a
Artyom Levykh <art.lv@hotmail.com>
2021-10-30 20:17:53 +0300

    Импорт библиотек (пояснения)

--- Пятнашки.py
+++ Пятнашки.py
@@ -1,5 +1,6 @@
-from tkinter import *
-from tkinter import ttk
-from tkinter import messagebox
-from random import randint
-from winsound import Beep
+from tkinter import *               # Общий Tkinter
+from tkinter import ttk             # Для использования RadioButton
+from tkinter import messagebox      # Для использования окон сообщений
+from random import randint          # Случайные числа
+from winsound import Beep           # Простейший генератор звука
+
