337149fe44fce3b7a5c57a7da99c4931e90b808c
Artyom Levykh <art.lv@hotmail.com>
2021-11-06 13:08:48 +0200

    Базовые настройки виджета Canvas

--- Sokoban/Сокобан.py
+++ Sokoban/Сокобан.py
@@ -1,2 +1,12 @@
 from tkinter import *
 
+root = Tk()
+root["bg"] = "#2F3F4F"                                   # Цвет фона
+root.geometry("480x360")                                 # Размер основного окна
+
+cnv = Canvas(root, width=240, height=180, bg="#AAAAAA")  # Создаём виджет Canvas с размерами width & height
+cnv.config(highlightthickness=0)                         # Убираем белую рамку вокруг окна Canvas
+cnv.place(x=50, y=50)                                    # Устанавливаем в область основного окна с отступом x, y
+
+
+root.mainloop()
