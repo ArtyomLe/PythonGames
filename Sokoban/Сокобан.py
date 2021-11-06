f8815962d5bc60cb18cd9b656e49fafab9a53e8f
Artyom Levykh <art.lv@hotmail.com>
2021-11-06 13:56:16 +0200

    Базовые настройки виджета Canvas 2

--- Sokoban/Сокобан.py
+++ Sokoban/Сокобан.py
@@ -1,12 +1,23 @@
 from tkinter import *
 
+WIDTH = 640
+HEIGHT = 480
+
 root = Tk()
-root["bg"] = "#2F3F4F"                                   # Цвет фона
-root.geometry("480x360")                                 # Размер основного окна
+root.geometry(f"{WIDTH}x{HEIGHT}")                     # Размер основного окна
 
-cnv = Canvas(root, width=240, height=180, bg="#AAAAAA")  # Создаём виджет Canvas с размерами width & height
-cnv.config(highlightthickness=0)                         # Убираем белую рамку вокруг окна Canvas
-cnv.place(x=50, y=50)                                    # Устанавливаем в область основного окна с отступом x, y
+cnv = Canvas(root, width=WIDTH, height=HEIGHT)         # Создаём виджет Canvas с размерами width & height
+cnv.config(highlightthickness=0)                       # Убираем белую рамку вокруг окна Canvas
+cnv.place(x=0, y=0)                                    # Устанавливаем в область основного окна без отступа (x=0, y=0)
+cnv.focus_set()                                        # Перехват нажатия клавиш с Canvas
 
+back = PhotoImage(file="background.png")               # Загружаем (изображение) как фоновую картинку
+cnv.create_image(WIDTH // 2, HEIGHT // 2, image=back)  # Привязываем картинку к левому верхнему углу(изначально она по центру)
+
+evilCircle = PhotoImage(file="circle.png")             # Загружаем красный круг
+evil = cnv.create_image(32, 32, image=evilCircle)      # Задаём координаты для размещения и помещаем в переменную(evil)
+
+playerSquare = PhotoImage(file="square.png")           # Загружаем зелёный квадрат
+player = cnv.create_image(WIDTH // 2, HEIGHT // 2, image=playerSquare)  # Создаём переменную player в начале координат
 
 root.mainloop()
