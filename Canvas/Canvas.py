8fd4c8acf257d96b1a6ad5d4e4b4333310ca1b46
Artyom Levykh <art.lv@hotmail.com>
2021-11-09 22:26:21 +0200

    Убеги от круга

--- Canvas/Canvas.py
+++ Canvas/Canvas.py
@@ -1,30 +1,70 @@
 from tkinter import *
-from time import sleep
 
-def pressSpace(event):
-    global countAnimation
-    cnv.unbind("<space>")                          # Не обрабатываем пробел
-    countAnimation += 1
-    if (countAnimation < 20):
-        cnv.move(evil, 2, 0)
-        root.after(50, lambda e=event: pressSpace(e))
+def move(vector):                               # Перемещение квадрата игрока
+    if (vector == UPKEY):
+        cnv.move(player, 0, -playerSpeed)
+    elif (vector == DOWNKEY):
+        cnv.move(player, 0, playerSpeed)
+    elif (vector == LEFTKEY):
+        cnv.move(player, -playerSpeed, 0)
+    elif (vector == RIGHTKEY):
+        cnv.move(player, playerSpeed, 0)
+
+def evilMove():                                 # Перемещение вражеского круга
+    global evilAfter, vectorX, vectorY
+    cnv.move(evil, vectorX, vectorY)
+    x = cnv.coords(evil)[0]
+    y = cnv.coords(evil)[1]
+
+    if (x > WIDTH - 32 or x < 32):
+        vectorX = -vectorX
+    if (y > HEIGHT - 32 or y < 32):
+        vectorY = -vectorY
+
+    xP = cnv.coords(player)[0]
+    yP = cnv.coords(player)[1]
+
+    distance = (abs(x - xP) ** 2 + abs(y - yP) ** 2) ** 0.5
+
+    if(distance < 64):
+        root.after_cancel(evilAfter)
     else:
-        countAnimation = 0
-        cnv.bind("<space>", pressSpace)              # Вновь задаём реакцию на нажатие клавиши
+        evilAfter = root.after(30, evilMove)
+
+WIDTH = 640
+HEIGHT = 480
 
 root = Tk()
-root.geometry(f"{640}x{480}")
+root.geometry(f"{WIDTH}x{HEIGHT}")
 
-cnv = Canvas(root, width=640, height=480)
+cnv = Canvas(root, width=WIDTH, height=HEIGHT)
 cnv.config(highlightthickness=0)
 cnv.place(x=0, y=0)
 cnv.focus_set()
 
+back = PhotoImage(file="background.png")
 evilCircle = PhotoImage(file="circle.png")
-evil = cnv.create_image(120, 240, image=evilCircle)
+playerSquare = PhotoImage(file="square.png")
 
-cnv.bind("<space>", pressSpace)
+cnv.create_image(WIDTH // 2, HEIGHT // 2, image=back)
 
-countAnimation = 0                                  # Кол-во рекурсий
+vectorX = 5
+vectorY = 5
 
+playerSpeed = 10
+
+evil = cnv.create_image(32, 32, image=evilCircle)
+player = cnv.create_image(WIDTH // 2, HEIGHT // 2, image=playerSquare)
+
+UPKEY = 0
+DOWNKEY = 1
+LEFTKEY = 2
+RIGHTKEY = 3
+
+cnv.bind("<Up>", lambda e, x = UPKEY: move(x))
+cnv.bind("<Down>", lambda e, x = DOWNKEY: move(x))
+cnv.bind("<Left>", lambda e, x = LEFTKEY: move(x))
+cnv.bind("<Right>", lambda e, x = RIGHTKEY: move(x))
+
+evilAfter = root.after(30, evilMove)
 root.mainloop()
