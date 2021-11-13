97fb35fcafd81449e5499319c97c4ed7e85a80da
Artyom Levykh <art.lv@hotmail.com>
2021-11-13 21:22:05 +0200

    Обьекты PhotoImage с загруженными из файлов изображениями

--- Sokoban/Сокобан.py
+++ Sokoban/Сокобан.py
@@ -16,15 +16,14 @@ SQUARE_SIZE = 64
 POS_X = root.winfo_screenwidth() // 2 - (WIDTH * SQUARE_SIZE) // 2
 POS_Y = root.winfo_screenheight() // 2 - (HEIGHT * SQUARE_SIZE) // 2
 root.geometry(f"{WIDTH * SQUARE_SIZE + 0}x{HEIGHT * SQUARE_SIZE + 0}+{POS_X}+{POS_Y}")
-print(POS_Y)
-print(POS_X)
-# Устанавливаем клавиши управления
+
+# Константы для направления движения ("Коды нажатых клавиш")
 UPKEY = 0
 DOWNKEY = 1
 LEFTKEY = 2
 RIGHTKEY = 3
 
-# Накладываем полотно Canvas на основную рамку
+# Создаём полотно для вывода графики
 cnv = Canvas (root, width=WIDTH * SQUARE_SIZE, height=HEIGHT * SQUARE_SIZE, bg="#373737")
 cnv.config(highlightthickness=0)
 cnv.place(x=0, y=0)
@@ -39,15 +38,16 @@ cnv.bind("<Right>", lambda e, x=RIGHTKEY: move(x))
 moving = True
 backGround = PhotoImage(file="image/grass.png")
 
+# Данный список содержит обьекты PhotoImage с загруженными из файлов изображениями
 img = []
-img.append(PhotoImage(file="image/wall.png"))
-img.append(PhotoImage(file="image/box.png"))
-img.append(PhotoImage(file="image/finish.png"))
+img.append(PhotoImage(file="image/wall.png"))               # img[0] - изображение стены
+img.append(PhotoImage(file="image/box.png"))                # img[1] - изображение ящика
+img.append(PhotoImage(file="image/finish.png"))             # img[2] - изображение точки сбора
 img.append([])
-img[3].append(PhotoImage(file="image/kosoban_up.png"))
-img[3].append(PhotoImage(file="image/kosoban_down.png"))
-img[3].append(PhotoImage(file="image/kosoban_left.png"))
-img[3].append(PhotoImage(file="image/kosoban_right.png"))
+img[3].append(PhotoImage(file="image/kosoban_up.png"))      # img[3][0] - изображение погрузчика, направленного вверх
+img[3].append(PhotoImage(file="image/kosoban_down.png"))    # img[3][1] - изображение погрузчика, направленного вниз
+img[3].append(PhotoImage(file="image/kosoban_left.png"))    # img[3][2] - изображение погрузчика, направленного влево
+img[3].append(PhotoImage(file="image/kosoban_right.png"))   # img[3][3] - изображение погрузчика, направленного вправо
 
 player = None
 boxes = None
