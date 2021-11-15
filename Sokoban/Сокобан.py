37fc09bcb7431753a98e0f7934158379a6ba2b0c
Artyom Levykh <art.lv@hotmail.com>
2021-11-15 20:53:08 +0200

    Проверка значений списков     print(finish)     print(player)     print(boxes) дополнения

--- Sokoban/Сокобан.py
+++ Sokoban/Сокобан.py
@@ -69,6 +69,7 @@ def createLevel():
                 cnv.create_image(SQUARE_SIZE // 2 + j * SQUARE_SIZE, SQUARE_SIZE // 2 + i * SQUARE_SIZE, image=img[0])
             elif (dataLevel[i][j] == 3):# (Счётчик j отвечает за координату x) (Счётчик i отвечает за координату y)
                 dataLevel[i][j] = 0     # Так как строка и столбец в двумерном списке это не (x,y) то координаты из списка надо приводить к координатам окна
+                # Информационный объект - список finish (Точки сбора ящиков)
                 finish.append([i, j, cnv.create_image(SQUARE_SIZE // 2 + j * SQUARE_SIZE, SQUARE_SIZE // 2 + i * SQUARE_SIZE, image=img[2]), False])
     # Проверяем файл уровня.dat на наличие значений 2(Ящик), 4(Погрузчик)
     # Второй цикл нужен для того, чтобы погрузчик и ящики гарантированно прорисовывались поверх точек сбора
@@ -76,10 +77,11 @@ def createLevel():
         for j in range(len(dataLevel[i])):
             if (dataLevel[i][j] == 2):
                 dataLevel[i][j] = 0
+                # Информационный объект - список boxes (Ящики)
                 boxes.append([i, j, cnv.create_image(SQUARE_SIZE // 2 + j * SQUARE_SIZE, SQUARE_SIZE // 2 + i * SQUARE_SIZE, image=img[1])])
             elif (dataLevel[i][j] == 4):
                 dataLevel[i][j] = 0
-                # Информационный объект - список player                         image=img[3][1] - это изображение погрузчика направленного вниз
+                # Информационный объект - список player (Погрузчик)             image=img[3][1] - это изображение погрузчика направленного вниз
                 player.append([i, j, cnv.create_image(SQUARE_SIZE // 2 + j * SQUARE_SIZE, SQUARE_SIZE // 2 + i * SQUARE_SIZE, image=img[3][1])])
     print(finish)
     print(player)
