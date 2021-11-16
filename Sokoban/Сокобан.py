6c1cca8ebfb14887d45f0fc441654d49edf640ac
Artyom Levykh <art.lv@hotmail.com>
2021-11-16 20:43:09 +0200

    Проверить клетку на доступность перемещения

--- Sokoban/Сокобан.py
+++ Sokoban/Сокобан.py
@@ -120,6 +120,66 @@ def updateText():
     textTime = cnv.create_text(10, 10, fill="#F7F668", anchor="nw", text=txt, font="Verdana, 13")
     timeRun = root.after(1000, updateText)
 
+# Проверить клетку на доступность перемещения
+def move(v):
+    print("Метод move()")
+    if (moving):
+        return 0
+    cnv.delete(player[2])
+    player[2] = cnv.create_image(SQUARE_SIZE // 2 + player[1] * SQUARE_SIZE, SQUARE_SIZE // 2 + player[0] * SQUARE_SIZE, image=img[3][v])
+    x = player[0]
+    y = player[1]
+    Beep(625, 10)
+
+    if (v == UPKEY):
+        check = getNumber(x - 1, y)
+        if (check == 0):
+            movePlayerTo(0, -8, 8)
+            player[0] -= 1
+        elif (check == 2):
+            nextCheck = getNumber(x - 2, y)
+            if (nextCheck == 0):
+                numberBox = getBox(x - 1, y)
+                movePlayerBoxTo(0, -8, 8, numberBox)
+                player[0] -= 1
+                boxes[numberBox][0] -= 1
+    elif (v == DOWNKEY):
+        check = getNumber(x + 1, y)
+        if (check == 0):
+            movePlayerTo(0, 8, 8)
+            player[0] += 1
+        elif (check == 2):
+            nextCheck = getNumber(x + 2, y)
+            if (nextCheck == 0):
+                numberBox = getBox(x + 1, y)
+                movePlayerBoxTo(0, 8, 8, numberBox)
+                player[0] += 1
+                boxes[numberBox][0] += 1
+    elif (v == LEFTKEY):
+        check = getNumber(x, y - 1)
+        if (check == 0):
+            movePlayerTo(-8, 0, 8)
+            player[1] -= 1
+        elif (check == 2):
+            nextCheck = getNumber(x, y - 2)
+            if (nextCheck == 0):
+                numberBox = getBox(x, y - 1)
+                movePlayerBoxTo(-8, 0, 8, numberBox)
+                player[1] -= 1
+                boxes[numberBox][1] -= 1
+    elif (v == RIGHTKEY):
+        check = getNumber(x, y + 1)
+        if (check == 0):
+            movePlayerTo(8, 0, 8)
+            player[1] += 1
+        elif (check == 2):
+            nextCheck = getNumber(x, y + 2)
+            if (nextCheck == 0):
+                numberBox = getBox(x, y + 1)
+                movePlayerBoxTo(8, 0, 8, numberBox)
+                player[1] += 1
+                boxes[numberBox][1] += 1
+
 
 # ================== НАЧАЛО ПРОГРАММЫ  =============================
 # Настраиваем основное окно(размеры, заголовок, расположение)
