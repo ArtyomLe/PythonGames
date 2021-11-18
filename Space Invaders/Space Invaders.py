37eb9e4fbdbb87816c48367e658e03dca02c67a8
Artyom Levykh <art.lv@hotmail.com>
2021-11-18 22:28:58 +0200

    Создание окна

--- Space Invaders/Space Invaders.py
+++ Space Invaders/Space Invaders.py
@@ -3,6 +3,8 @@ from random import randint
 from time import sleep
 from winsound import Beep
 
+#============================================================================
+
 # Создание окна
 root = Tk()
 root.resizable(False, False)
@@ -15,3 +17,13 @@ SQUARE_SIZE = 32
 POS_X = root.winfo_screenwidth() // 2 - WIDTH // 2
 POS_Y = root.winfo_screenheight() // 2 - HEIGHT // 2
 root.geometry(f"{WIDTH}x{HEIGHT}+{POS_X}+{POS_Y}")
+
+cnv = Canvas(root, width=WIDTH, height=HEIGHT, bg="#000000")
+cnv.config(highlightthickness=0)
+cnv.place(x=0, y=0)
+
+backGround = PhotoImage(file="image/background.png")
+
+# ===========================================================
+
+root.mainloop()
