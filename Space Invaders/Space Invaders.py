5a632ac0748e4470dab41bd62032ee08b1434303
Artyom Levykh <art.lv@hotmail.com>
2021-11-19 22:18:55 +0200

    Сброс и формирование объектов игрового мира (def reset)

--- Space Invaders/Space Invaders.py
+++ Space Invaders/Space Invaders.py
@@ -4,6 +4,60 @@ from time import sleep
 from winsound import Beep
 
 #============================================================================
+# Сброс всего подчистую с установкой первого уровня
+def globalReset():
+    global level, score, penalty, playGame, playerSpeed, lives
+    playGame = False
+    playerSpeed = 5
+    level = 1
+    score = 0
+    penalty = 0
+    lives = 3
+
+# Перезапуск игры полностью
+def restartGame():
+    globalReset()
+    reset()
+    showScores(-1)
+
+# Сброс и формирование объектов игрового мира
+def reset()
+    global invadersObject, invadersWidth, invadersHeight, invadersSpeed, leftInvadersBorder, rightInvadersBorder, player, maxY, rocketObject, invadersRocket
+
+    cnv.delete(ALL)
+    cnv.create_image(WIDTH // 2, HEIGHT // 2, image=backGround)
+    cnv.focus_set()
+
+    rocketObject = None
+    invadersRocket = None
+    invadersSpeed = 3 + level // 5              # Чем выше уровень тем быстрее перемещаются пришельцы
+
+    invadersWidth = (1 + int(level // 3)) * 2
+    invadersHeight = 2 + (level // 4)
+    if (invadersWidth > 14):                    # Ограничиваем ширину в 14 единиц в независимости от повышения уровня
+        invadersWidth = 14
+    if (invadersHeight > 8):                    # Ограничиваем высоту в 8 единиц в независимости от повышения уровня
+        invadersHeight = 8
+
+    maxY = (invadersHeight - 1) * 10 + SQUARE_SIZE * invadersHeight + SQUARE_SIZE // 2
+
+    invadersObject = []
+    for i in range(invadersWidth):
+        for j in range(invadersHeight):
+            rang = randint(0, level // 8)
+            if (rang > 2):
+                rang = 2
+            posX = SQUARE_SIZE // 2 + (WIDTH // 2 - (invadersWidth * (SQUARE_SIZE + 10)) // 2) + i * SQUARE_SIZE + i * 10
+            posY = 20 + j * 10 + j * SQUARE_SIZE
+            invadersObject.append([cnv.create_image(posX, posY, image=invadersTexture[rang * 2]), rang])
+
+    leftInvadersBorder = cnv.coords(invadersObject[0][0])[0]
+    rightInvadersBorder = cnv.coords(invadersObject[len(invadersObject) - 1][0])[0]
+
+    player = [cnv.create_image(WIDTH // 2, HEIGHT - SQUARE_SIZE * 2, image=playerTexture), 1]
+
+    updateInfoLine()
+    mainloop()
 
 # Создание окна
 root = Tk()
@@ -91,7 +145,7 @@ menu1["command"] = startGame
 
 menu2 = Button(root, text="Сброс", font=", 20", width=20)
 menu2.place(x=-100, y=-100)
-menu2["command"] = restartGame
+menu2["command"] = restartGame          # Перезапуск игры полностью
 
 btnContinueAfterPause = None
 
@@ -103,8 +157,8 @@ textScores = None
 informationLine = None
 
 # ===НАЧАЛО===============================================
-globalReset()
-reset()
+globalReset()       # Сброс всего подчистую с установкой первого уровня
+reset()             # Сброс и формирование объектов игрового мира
 playGame = True
 mainloop()
 
